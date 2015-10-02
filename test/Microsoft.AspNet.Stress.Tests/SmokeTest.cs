131ea13e15883c24a6d80355fb2830919c415114
Pranav K <prkrishn@hotmail.com>
2015-10-02 16:19:35 -0700

    Switch tests to use aspnet.xunit.performance

--- test/Microsoft.AspNet.Stress.Tests/SmokeTest.cs
+++ test/Microsoft.AspNet.Stress.Tests/SmokeTest.cs
@@ -2,44 +2,34 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
-using Microsoft.AspNet.StressFramework;
 using Microsoft.AspNet.WebUtilities;
-using Xunit;
+using Microsoft.Xunit.Performance;
 
 namespace Microsoft.AspNet.Stress.Tests
 {
     public class SmokeTest
     {
-        [StressTest]
-        public StressRunSetup SmokeyMcSmokeTest()
+        [Benchmark]
+        [MeasureInstructionsRetired]
+        public void SmokeyMcSmokeTest()
         {
+            // Arrange
             var formData = "first=second&third=fourth&fifth=sixth";
             var formReader = new FormReader(formData);
             var pairs = new List<KeyValuePair<string, string>>();
             var cancellationToken = new CancellationToken();
-            var expectedPairs = new Dictionary<string, string>
-                    {
-                        { "first", "second" },
-                        { "third", "fourth" },
-                        { "fifth", "sixth" },
-                    }.ToList();
 
-            return StressRunSetup.CreateTest(
-                async () =>
+            // Act
+            Benchmark.IterateAsync(async () =>
+            {
+                var pair = await formReader.ReadNextPairAsync(cancellationToken);
+                while (pair != null)
                 {
-                    // Act
-                    var pair = await formReader.ReadNextPairAsync(cancellationToken);
-                    while (pair != null)
-                    {
-                        pairs.Add(pair.Value);
-                        pair = await formReader.ReadNextPairAsync(cancellationToken);
-                    }
-
-                    // Assert
-                    Assert.Equal(expectedPairs, pairs);
-                });
+                    pairs.Add(pair.Value);
+                    pair = await formReader.ReadNextPairAsync(cancellationToken);
+                }
+            });
         }
     }
 }
